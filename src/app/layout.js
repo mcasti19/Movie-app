86b9edfae33ecf8721348bc9c10b3d5f1311b70c
mcasti19 <castdevel@gmail.com>
2024-11-11 20:23:09 -0400

    dark mode added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,31 +1,23 @@
-import localFont from "next/font/local";
-import "./globals.css";
-import { Header } from "@/components/Header";
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { Header } from '@/components/Header';
+import { Providers } from './Providers';
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter( { subsets: ['latin'] } );
 
 export const metadata = {
-  title: "My Movie Club",
-  description: "This is a Movie Database clone",
+  title: 'IMDb clone',
+  description: 'This is a movie database clone',
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout ( { children } ) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Header />
-        {children}
+    <html lang='en'>
+      <body className={ inter.className }>
+        <Providers>
+          <Header />
+          { children }
+        </Providers>
       </body>
     </html>
   );
